9ecf89ec6be3662dc58051d6a524042a8de535b7
Artemi Bogatov <aboga99@mail.ru>
2025-03-13 18:02:56 +0300

    Верстка главной страницы. Начало реализации POST-запроса в веб приложении.

--- app.py
+++ app.py
@@ -1,40 +1,24 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
+from flask import Flask, request
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Открываем файл contacts.html и читаем его содержимое
-        with open('contacts.html', 'r', encoding='utf-8') as file:
-            html_content = file.read()
+app = Flask(__name__)
 
-        # Устанавливаем заголовок Content-type
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+@app.route('/', methods=['GET'])
+def home():
+    with open('index.html', 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content, 200, {'Content-Type': 'text/html'}
 
-        # Отправляем содержимое файла
-        self.wfile.write(html_content.encode('utf-8'))
+@app.route('/contacts', methods=['GET'])
+def contacts():
+    with open('contacts.html', 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content, 200, {'Content-Type': 'text/html'}
 
-    def do_POST(self):
-        # Получаем длину данных
-        content_length = int(self.headers['Content-Length'])
-        # Читаем данные из запроса
-        post_data = self.rfile.read(content_length)
-
-        # Выводим данные в консоль
-        print(f"Получены данные: {post_data.decode('utf-8')}")
-
-        # Отправляем ответ
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b'POST-запрос успешно обработан')
-
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8080):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Сервер запущен на порту {port}...')
-    httpd.serve_forever()
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form
+    print("Полученные данные:", data)
+    return "Данные получены", 200
 
 if __name__ == '__main__':
-    run()
+    app.run(debug=True)
